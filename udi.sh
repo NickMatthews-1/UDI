f28a65d31774e75a7524c4fc6cb74e77926a980d
Nick Matthews <matthews3102000@gmail.com>
2022-02-02 18:08:04 -0500

    First file

---
+++ udi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#This downloads and installs Ubuntu desktop
+sudo apt-get install -y ubuntu-desktop
+
+#This downloads and installs xrdp
+sudo apt-get install xrdp
+
+#This downloads and installs xfce4
+sudo apt-get install xfce4
+
+#This downloads and installs xfce4-goodies
+sudo apt-get install xfce4-goodies
+
+#This updates everything
+sudo apt update
+
+#Subject to GPLv3 License Copyright Nicholas Matthews 2022
