aafe38a946efd94c7b84d662e05b68f44acceb53
Nick Matthews <matthews3102000@gmail.com>
2022-02-05 06:10:35 -0500

    Update udi.sh

--- udi.sh
+++ udi.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
-
-#This downloads and installs Ubuntu desktop
 sudo apt install -y ubuntu-desktop
-
-#This downloads and installs xfce4
 sudo apt install xfce4
-
-#This downloads and installs xfce4-goodies
 sudo apt install xfce4-goodies
-
-#This updates everything
 sudo apt update
-
-#Subject to GPLv3 License Copyright Nicholas Matthews 2022
